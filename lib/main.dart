3cff5bef78fc88a9332eb4536e030391c6fc0ad1
Yousuf Hossain Siam <yousufhossainsiam@gmail.com>
2025-01-13 23:43:37 +0600

    title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fluttedifhdjhfr all..................fhdfhd',
+      title: 'siam',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
