2388fe3b0da459d6ff3181ce67c461dad33a5fe2
jihad1236 <jihadmugdh0000@gmail.com>
2025-01-13 22:32:51 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,12 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // This widget is the root of your application.siam
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Fluttedifhdjhfr all',
+      title: 'Fluttedifhdjll',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
